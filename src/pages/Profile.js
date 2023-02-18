897255f310072f110b29afb25acc10e6894dde7e
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2023-02-18 16:32:13 +0100

    feat: show the profile's posts in its page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,36 @@
+import { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
+import { fetchAllPostsByUser, getUser } from '../lib/api';
+import PostList from '../containers/PostList';
+import { Col, Row } from 'antd';
 
 function Profile() {
   const params = useParams();
+  const [profile, setProfile] = useState();
+  const [posts, setPosts] = useState();
 
-  return (
-    <div>
-      <h1>Profile {params.id}</h1>
-    </div>
-  )
+  useEffect(() => {
+    if (!profile) getUser(params.id).then(user => setProfile(user));
+  }, [profile, params.id]);
+
+  useEffect(() => {
+    if (profile && !posts) {
+      fetchAllPostsByUser(profile._id)
+        .then((userPosts) => setPosts(userPosts));
+    }
+  }, [profile, posts]);
+
+  return profile ? (
+      <Row gutter={16}>
+        <Col span={12}>
+          <h1>{profile.firstname} {profile.lastname}'s profile</h1>
+
+        </Col>
+        <Col span={12}>
+          <PostList posts={posts || []} />
+        </Col>
+      </Row>
+  ) : null;
 }
 
 export default Profile;
